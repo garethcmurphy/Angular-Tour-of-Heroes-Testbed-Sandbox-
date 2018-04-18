8cc8f50ae55443e9fe830d9ad3ef68de5b96faa8
Gareth Murphy <garethcmurphy@gmail.com>
2018-04-18 14:44:03 +0200

    tets file upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FileHelpersModule } from 'ngx-file-helpers';
+
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +12,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+	FileHelpersModule
   ],
   providers: [],
   bootstrap: [AppComponent]
